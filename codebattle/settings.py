c4e5c9e6f8781437fbe800a449f84786d627a3ce
unknown <pollo_10000@hotmail.com>
2022-11-07 13:50:04 -0600

    pre deploy3

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -152,5 +152,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000/",
-    "pagina-registro-production.up.railway.app",
+    "https://pagina-registro-production.up.railway.app/",
 ]
