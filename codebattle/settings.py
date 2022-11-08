cab01c2de45c827da22b5db4ae85523ac71e54dc
unknown <pollo_10000@hotmail.com>
2022-11-07 23:55:34 -0600

    new domain

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -152,5 +152,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "https://jmcasados-eventos.xyz/",
+    "https://jmcasados-eventos.xyz",
 ]
